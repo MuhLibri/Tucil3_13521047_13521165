941fa45da1306dfb29f934ebb1f0965a76e3e842
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2023-04-08 11:48:02 +0700

    Add drawGraph to Utils

--- src/Utils.py
+++ src/Utils.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Utils:
@@ -14,21 +17,65 @@ class Utils:
                     return "not_number"
         return "valid"
 
-    def readFile() -> list[list[int]]:
+    def readFile() -> list[list[int]] and bool:
         root = tk.Tk()
         root.withdraw()
         file = filedialog.askopenfile()
-        adjecencyMatrix = [[num.strip() for num in line.split(',')] for line in file]
-        status = Utils.checkValid(adjecencyMatrix)
+        adjacencyMatrix = [[num.strip() for num in line.split(',')] for line in file]
+        file.close()
+        status = Utils.checkValid(adjacencyMatrix)
+        valid : bool
 
         if (status == "valid"):
-            adjecencyMatrix = [list(map(int, x)) for x in adjecencyMatrix]
+            adjacencyMatrix = [list(map(int, x)) for x in adjacencyMatrix]
+            print("File valid")
+            valid = True
         elif (status == "too_small"):
             print("Ukuran matrix minimal 8 x 8")
+            valid = False
         elif (status == "size_diff"):
             print("Ukuran baris dan kolom harus sama")
+            valid = False
         else:
             print("Matrix hanya dapat berupa angka")
+            valid = False
 
-        file.close()
-        return adjecencyMatrix
+        return adjacencyMatrix, valid
+
+    def nameNode(manual : bool, nodeAmount : int) -> dict[str,int] and list[str]:
+        nodeTupleList : list[(str,int)] = []
+        nameList : list[str] = []
+        
+        if (manual):
+            for i in range (nodeAmount):
+                name = input()
+                nameList.append(name)
+                nodeTupleList.append((name,i))
+        else:
+            for i in range (nodeAmount):
+                name = str(i+1)
+                nameList.append(name)
+                nodeTupleList.append((name,i))        
+        nodeDict = dict(nodeTupleList)
+
+        return nodeDict, nameList
+
+    def drawGraph(adjacencyMatrix: list[list[int]], nameList : list[str]):
+        G = nx.Graph()
+        for i in range (len(nameList)):
+            for j in range (len(adjacencyMatrix[i])):
+                if (adjacencyMatrix[i][j] != 0):
+                    G.add_edge(nameList[i], nameList[j], weight = adjacencyMatrix[i][j])
+
+        pos = nx.spring_layout(G, seed=7)
+        nx.draw_networkx_nodes(G, pos, node_size=6000)
+        nx.draw_networkx_edges(G, pos, edgelist=G.edges, width=10)
+        nx.draw_networkx_labels(G, pos, font_size=25, font_family="sans-serif")
+        edge_labels = nx.get_edge_attributes(G, "weight")
+        nx.draw_networkx_edge_labels(G, pos, edge_labels, font_size=20)
+
+        ax = plt.gca()
+        ax.margins(0.08)
+        plt.axis("off")
+        plt.tight_layout()
+        plt.show()
--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 from Utils import *
 
-graf = Utils.readFile()
-print(graf)
+adjacencyMatrix, valid = Utils.readFile()
+while (not valid):
+    print("Silahkan Masukkan file yang valid")
+    adjacencyMatrix, valid = Utils.readFile()
+
+
+nodeDict,nameList = Utils.nameNode(False, 8)
+Utils.drawGraph(adjacencyMatrix, nameList)
