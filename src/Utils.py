a30b51c9f1265d9d265f2093b9b769f13fd2eb0b
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2023-04-12 19:17:41 +0700

    Add coordinate to readFile

--- src/Utils.py
+++ src/Utils.py
@@ -7,42 +7,50 @@ import numpy as np
 
 class Utils:
     def checkValid(adjecencyMatrix: list[list[str]]) -> str:
-        if (len(adjecencyMatrix)-1 < 8 or len(adjecencyMatrix[0]) < 8):
+        if (len(adjecencyMatrix)-2 < 8 or len(adjecencyMatrix[0]) < 8):
             return "too_small"
-        elif (len(adjecencyMatrix)-1 != len(adjecencyMatrix[0])):
+        elif (len(adjecencyMatrix)-2 != len(adjecencyMatrix[0])):
             return "size_diff"
-        for i in range (len(adjecencyMatrix)-1):
+        elif (len(adjecencyMatrix[len(adjecencyMatrix)-2]) != len(adjecencyMatrix[0])):
+            return "name_not_enough"
+        elif (len(adjecencyMatrix[len(adjecencyMatrix)-1]) != len(adjecencyMatrix[0])):
+            return "coordinate_not_enough"
+        for i in range (len(adjecencyMatrix)-2):
             for j in range (len(adjecencyMatrix[0])):
                 if (not adjecencyMatrix[i][j].isdigit()):
                     return "not_number"
         return "valid"
 
-    def readFile() -> list[list[int]] and list[str] and bool:
+    def readFile() -> list[list[int]] and list[str] and list[(int,int)] and bool:
         root = tk.Tk()
         root.withdraw()
         file = filedialog.askopenfile()
-        adjacencyMatrix = [[num.strip() for num in line.split(',')] for line in file]
-        nameList = adjacencyMatrix[len(adjacencyMatrix)-1]
+        adjacencyMatrix = [[num.strip() for num in line.split(' ')] for line in file]
+        nameList = adjacencyMatrix[len(adjacencyMatrix)-2]
+        coordinateList = [eval(elmt) for elmt in adjacencyMatrix[len(adjacencyMatrix)-1]]
         file.close()
         status = Utils.checkValid(adjacencyMatrix)
         valid : bool
 
         if (status == "valid"):
-            adjacencyMatrix = adjacencyMatrix[0:len(adjacencyMatrix)-1]
+            valid = True
+            adjacencyMatrix = adjacencyMatrix[0:len(adjacencyMatrix)-2]
             adjacencyMatrix = [list(map(int, x)) for x in adjacencyMatrix]
             print("File valid")
-            valid = True
-        elif (status == "too_small"):
-            print("Ukuran matrix minimal 8 x 8")
-            valid = False
-        elif (status == "size_diff"):
-            print("Ukuran baris dan kolom harus sama")
-            valid = False
         else:
-            print("Matrix hanya dapat berupa angka")
             valid = False
+            if (status == "too_small"):
+                print("Ukuran matrix minimal 8 x 8")
+            elif (status == "size_diff"):
+                print("Ukuran baris dan kolom harus sama")
+            elif (status == "name_not_enough"):
+                print("Jumlah nama tidak sesuai dengan jumlah simpul")
+            elif (status == "coordinate_not_enough"):
+                print("Jumlah koordinat tidak sesuai dengan jumlah simpul")
+            else:
+                print("Matrix hanya dapat berupa angka")
 
-        return adjacencyMatrix, nameList, valid
+        return adjacencyMatrix, nameList, coordinateList, valid
 
     def matrixToMap(adjacencyMatrix: list[list[int]], nameList : list[str]) -> dict[(str,list[(str,int)])]:
         graphMap : dict
@@ -83,8 +91,13 @@ class Utils:
         plt.tight_layout()
         plt.show()
 
-    def showPath(adjacencyMatrix: list[list[int]], nameList : list[str], path : list[str]):
+    def showPath(adjacencyMatrix: list[list[int]], nameList : list[str], coordinateList : list[(int,int)], path : list[str]):
         G = nx.Graph()
+        i = 0
+        for name in nameList:
+            G.add_node(name, position=(coordinateList[i][0], coordinateList[i][1]))
+            i += 1
+
         for i in range (len(nameList)):
             for j in range (len(adjacencyMatrix[i])):
                 if (adjacencyMatrix[i][j] != 0):
@@ -95,7 +108,7 @@ class Utils:
             ePath.append((path[i], path[i+1]))
         eNotPath = [(u, v) for (u, v, d) in G.edges(data=True) if (u, v) not in ePath]
 
-        pos = nx.spring_layout(G, seed=7)
+        pos = nx.get_node_attributes(G, 'position')
         nx.draw_networkx_nodes(G, pos, node_size=[len(v) * 3000 for v in G.nodes()])
 
         nx.draw_networkx_edges(G, pos, edgelist=eNotPath, width=10)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 from Utils import *
 from UCS import *
 
-adjacencyMatrix, nameList, valid = Utils.readFile()
+adjacencyMatrix, nameList, coordinateList, valid = Utils.readFile()
 while (not valid):
     print("Silahkan Masukkan file yang valid")
-    adjacencyMatrix, nameList, valid = Utils.readFile()
+    adjacencyMatrix, nameList, coordinateList, valid = Utils.readFile()
 
-#                   1      2      3      4      5      6      7      8
-coordinateList = [(0,0),(0,-11),(2,0),(13,13),(0,10),(3,13),(0,20),(0,13)]
+
 Utils.drawGraph(adjacencyMatrix, nameList, coordinateList)
 map = Utils.matrixToMap(adjacencyMatrix, nameList)
 
@@ -16,4 +15,4 @@ destination = input("Masukkan simpul tujuan: ")
 distance, path = UCS.findUCS(origin, destination, map, nameList)
 print("Jarak dari simpul", origin, "ke simpul", destination, "adalah", distance)
 
-Utils.showPath(adjacencyMatrix, nameList, path)
+Utils.showPath(adjacencyMatrix, nameList, coordinateList, path)
