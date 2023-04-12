cecd2eb9f5a4076b94bb0afa642b49e0a25ff5cb
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-04-12 23:41:52 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,46 @@
 from Utils import *
 from UCS import *
 from AStar import *
+from Map import *
 
-adjacencyMatrix, nameList, coordinateList, valid = Utils.readFile()
-while (not valid):
-    print("Silahkan masukkan file yang valid")
+graphMode = input('Masukkan mode yang diinginkan (peta/file)')
+if(graphMode=='file'):
     adjacencyMatrix, nameList, coordinateList, valid = Utils.readFile()
+    while (not valid):
+        print("Silahkan masukkan file yang valid")
+        adjacencyMatrix, nameList, coordinateList, valid = Utils.readFile()
 
 
-Utils.drawGraph(adjacencyMatrix, nameList, coordinateList)
-map = Utils.matrixToMap(adjacencyMatrix, nameList)
+    Utils.drawGraph(adjacencyMatrix, nameList, coordinateList)
+    map = Utils.matrixToMap(adjacencyMatrix, nameList)
 
-origin = input("Masukkan simpul asal: ")
-destination = input("Masukkan simpul tujuan: ")
+    origin = input("Masukkan simpul asal: ")
+    destination = input("Masukkan simpul tujuan: ")
 
-astar = AStar(map)
-distance, path = astar.solve(origin, destination)
-print(path)
+    searchMode = input('Masukkan mode searching(ucs/astar): ')
+    if(searchMode=='ucs'):
+        distance, path = UCS.findUCS(origin, destination, map, nameList)
+    elif(searchMode=='astar'):
+        astar = AStar(map)
+        distance, path = astar.solve(origin, destination)
+
+
+    print("Jarak dari simpul", origin, "ke simpul", destination, "adalah", distance)
+    Utils.showPath(adjacencyMatrix, nameList, coordinateList, path)
+elif(graphMode == 'peta'):
+    origin = input("Masukkan simpul asal: ")
+    destination = input("Masukkan simpul tujuan: ")
+
+    mappeta = Map(origin, destination, 'astar')
+
+    print("Jarak dari simpul", origin, "ke simpul", destination, "adalah", mappeta.distance)
+
+    mappeta.plot()
+
+
+
+# astar = AStar(map)
+# distance, path = astar.solve(origin, destination)
+# print(path)
 
 # distance, path = UCS.findUCS(origin, destination, map, nameList)
-print("Jarak dari simpul", origin, "ke simpul", destination, "adalah", distance)
-
-Utils.showPath(adjacencyMatrix, nameList, coordinateList, path)
